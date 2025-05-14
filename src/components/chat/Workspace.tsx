13eec4157f0a070b2fa0480eff7353e42a46f2b4
Pieli <49155289+Pieli@users.noreply.github.com>
2025-05-14 23:20:12 +0200

    Changed some navigation items and layouting

--- src/components/chat/Workspace.tsx
+++ src/components/chat/Workspace.tsx
@@ -26,6 +26,15 @@ import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
 
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
 
 interface CompostitionConfig {
   id: string;
@@ -85,11 +94,36 @@ const SequenceBuilder: React.FC<{ comps: CompostitionConfig[] }> = ({
 const Navbar = () => {
     return (
         <div className="flex items-center justify-between p-4 border-b m-0 text-secondary-foreground">
-        <h1 className="text-xl font-bold">Imagine</h1>
-        <ModeToggle />
-        <Avatar>
-            <AvatarFallback>PG</AvatarFallback>
-        </Avatar>
+            <DropdownMenu>
+                <DropdownMenuTrigger> 
+                    <span className="text-xl font-bold">Imagine</span>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                    <DropdownMenuLabel>My Projects</DropdownMenuLabel>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem>Files</DropdownMenuItem>
+                    <DropdownMenuItem>Exports</DropdownMenuItem>
+                    <DropdownMenuItem>Recents</DropdownMenuItem>
+                </DropdownMenuContent>
+            </DropdownMenu>
+            <div className="flex items-center gap-4">
+                <ModeToggle />
+                <DropdownMenu>
+                    <DropdownMenuTrigger> 
+                    <Avatar>
+                        <AvatarFallback>PG</AvatarFallback>
+                    </Avatar>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent>
+                        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem>Profile</DropdownMenuItem>
+                        <DropdownMenuItem>Billing</DropdownMenuItem>
+                        <DropdownMenuItem>Team</DropdownMenuItem>
+                        <DropdownMenuItem>Subscription</DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+                </div>
         </div>
     );
 }
