18c7ec1a42396e1dd304f6cf419f01533acaf509
Pieli <49155289+Pieli@users.noreply.github.com>
2025-05-19 09:38:10 +0200

    Added interface for composition configurations

---
+++ src/components/interfaces/compositions.ts
@@ -0,0 +1,12 @@
+import { z } from 'zod';
+
+export interface CompostitionConfig {
+  id: string;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  component: React.FC<any>;
+  schema: z.ZodTypeAny;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  props: Record<string, any>;
+  duration: number;
+}
+
