cefdb3a274a20adbb89cb3c675bc80b8faea2e51
Pieli <49155289+Pieli@users.noreply.github.com>
2025-05-22 16:44:19 +0200

    Updated Prompt

--- src/api/llm.ts
+++ src/api/llm.ts
@@ -32,10 +32,12 @@ export class OpenAIService implements LLMService {
 
   async generateCompositions(prompt: string): Promise<ResponseType> {
     const systemPrompt = (
-        'You are an AI that writes creates typography motion animations for promo videos' +
-        'Your goal is to craft the perfect script and animation for the user.' +
-        'Promo videos are stunning, engaging, unpredictable.' +  
+        'You are an expert AI Animator that writes creates typography motion animations for promo videos' +
+        'Your goal is to craft the perfect script and animation for the user and tell a story.' + 
+        'Promotional videos are delightful, playful, engaging, serendipitous.' +  
         'Content per Composition should be kept at a minimum and less is more.' +
+        'Only use 1-3 words per slide not more; if the content is longer it should be split in a meaningful way' + 
+        'The whole sum should tell a story and convice the audience of the topic/prodcut' +
         'You have the following animations available to choose from: ' +
         generateAnimationContext(bindings) +
         'Duration is in frames and the video has 30 FPS' +
