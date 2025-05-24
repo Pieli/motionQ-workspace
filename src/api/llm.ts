50c9e4f48007533170bfc40c8274442da37f4b0d
Pieli <49155289+Pieli@users.noreply.github.com>
2025-05-24 17:46:40 +0200

    Extracted the systemprompt into its own file

--- src/api/llm.ts
+++ src/api/llm.ts
@@ -2,25 +2,15 @@ import OpenAI from "openai";
 import { zodResponseFormat } from 'openai/helpers/zod';
 
 
-import { Response } from "./types";
+import { Response } from "@/api/types";
+import { systemPrompt }  from "@/api/system-prompt"
 
 import type { ResponseType } from "./types";
 import type { LLMService } from "@/components/interfaces/llm";
 import type { CompositionConfig } from "@/components/interfaces/compositions";
-import type { AnimationBinding } from "@/remotion-lib/animation-bindings";
 import type { AnimationComponents } from "@/api/animation-factories";
 
-
 import { animationFactory, schemaFactory } from "./animation-factories";
-import { bindings } from "@/remotion-lib/animation-bindings";
-
-
-export function generateAnimationContext(bindings: AnimationBinding[]): string {
-    return bindings.map(animation =>
-        `Animation: ${animation.name}\nUse case: ${animation.usecase}\nParameters: ${animation.settings}\n`
-    ).join('\n');
-}
-
 
 
 export class OpenAIService implements LLMService {
@@ -31,19 +21,6 @@ export class OpenAIService implements LLMService {
   }
 
   async generateCompositions(prompt: string): Promise<ResponseType> {
-    const systemPrompt = (
-        'You are an expert AI Animator that writes creates typography motion animations for promo videos' +
-        'Your goal is to craft the perfect script and animation for the user and tell a story.' +
-        'Promotional videos are delightful, playful, engaging, serendipitous.' +
-        'Content per Composition should be kept at a minimum and less is more.' +
-        'Only use 1-3 words per slide not more; if the content is longer it should be split in a meaningful way' +
-        'The whole sum should tell a story and convice the audience of the topic/prodcut' +
-        'You have the following animations available to choose from: ' +
-        generateAnimationContext(bindings) +
-        'Duration is in frames and the video has 30 FPS' +
-        'Output: JSON format with composition-array and commentary.'
-    );
-
     const completion = await this.client.beta.chat.completions.parse({
       model: 'gpt-4.1-nano-2025-04-14',
       messages: [{role: 'system', content: systemPrompt}, { role: 'user', content: prompt }],
