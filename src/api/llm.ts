ead00ced0c23f5bf677724caec0ba72414688d09
Pieli <49155289+Pieli@users.noreply.github.com>
2025-05-24 13:55:01 +0200

    changed model to cheaper option

--- src/api/llm.ts
+++ src/api/llm.ts
@@ -33,10 +33,10 @@ export class OpenAIService implements LLMService {
   async generateCompositions(prompt: string): Promise<ResponseType> {
     const systemPrompt = (
         'You are an expert AI Animator that writes creates typography motion animations for promo videos' +
-        'Your goal is to craft the perfect script and animation for the user and tell a story.' + 
-        'Promotional videos are delightful, playful, engaging, serendipitous.' +  
+        'Your goal is to craft the perfect script and animation for the user and tell a story.' +
+        'Promotional videos are delightful, playful, engaging, serendipitous.' +
         'Content per Composition should be kept at a minimum and less is more.' +
-        'Only use 1-3 words per slide not more; if the content is longer it should be split in a meaningful way' + 
+        'Only use 1-3 words per slide not more; if the content is longer it should be split in a meaningful way' +
         'The whole sum should tell a story and convice the audience of the topic/prodcut' +
         'You have the following animations available to choose from: ' +
         generateAnimationContext(bindings) +
@@ -45,7 +45,7 @@ export class OpenAIService implements LLMService {
     );
 
     const completion = await this.client.beta.chat.completions.parse({
-      model: 'gpt-4o',
+      model: 'gpt-4.1-nano-2025-04-14',
       messages: [{role: 'system', content: systemPrompt}, { role: 'user', content: prompt }],
       temperature: 0.7,
       max_tokens: 1500,
