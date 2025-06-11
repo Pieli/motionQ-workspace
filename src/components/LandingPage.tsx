ed5b6d0ff27644a1651988beb59bfd328f905e9c
Pieli <49155289+Pieli@users.noreply.github.com>
2025-06-11 17:31:42 +0200

    Added landing page sceleton

---
+++ src/components/LandingPage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const LandingPage: React.FC = () => {
+  return (
+    <div className="landing-page">
+      <h1>Welcome to Imagine</h1>
+      <p>Please log in to access your workspace.</p>
+      {/* Add login functionality or navigation */}
+    </div>
+  );
+};
+
+export default LandingPage;
