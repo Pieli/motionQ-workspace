bbbb2aadb4b4b65e0a04e507800afc8f7b64538c
Pieli <49155289+Pieli@users.noreply.github.com>
2025-07-15 15:37:12 +0200

    Fixed simple text typing

--- src/remotion-lib/TextFades/SimpleTextTyping.tsx
+++ src/remotion-lib/TextFades/SimpleTextTyping.tsx
@@ -7,7 +7,7 @@ import {
 } from "remotion";
 import { loadFont } from "@remotion/google-fonts/Inter";
 import type { SimpleTypingProps } from "./schemas";
-import { Typography } from "@/components/Typography/Typography";
+import { fitText } from "@remotion/layout-utils";
 
 loadFont("normal", {
   subsets: ["latin"],
@@ -32,6 +32,14 @@ export const SimpleTextTyping: React.FC<SimpleTypingProps> = (props) => {
 
   const visibleCharacters = Math.floor(progress * typoProps.text.length);
 
+  const maxWidth = 1536;
+  const { fontSize } = fitText({
+    text: typoProps.text,
+    withinWidth: maxWidth,
+    fontFamily: typoProps.fontFamily,
+    fontWeight: typoProps.fontWeight,
+  });
+
   const container: React.CSSProperties = {
     justifyContent: "center",
     alignItems: "center",
@@ -40,16 +48,30 @@ export const SimpleTextTyping: React.FC<SimpleTypingProps> = (props) => {
   const typingText = typoProps.text.split("").map((char, index) => {
     const opacity = index <= visibleCharacters ? 1 : 0;
     return (
-      <span key={index} style={{ opacity }}>{
-        char === " " ? "\u00A0" : char
-      }</span>
+      <span key={index} style={{ opacity }}>
+        {char === " " ? "\u00A0" : char}
+      </span>
     );
   });
 
   return (
     <AbsoluteFill style={outer}>
       <AbsoluteFill style={container}>
-        <Typography {...typoProps} text={typingText} />
+        <div
+          style={{
+            color: typoProps.textColor,
+            fontSize: typoProps.fontSize || fontSize,
+            fontWeight: typoProps.fontWeight,
+            fontFamily: typoProps.fontFamily,
+            textAlign: typoProps.textAlign,
+            width: maxWidth,
+            margin: "0 auto",
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          {typingText}
+        </div>
       </AbsoluteFill>
     </AbsoluteFill>
   );
