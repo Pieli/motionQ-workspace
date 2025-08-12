e1f6e7f26d1efcd0f79384d42c1d4e571ceeb559
Pieli <49155289+Pieli@users.noreply.github.com>
2025-08-12 11:54:38 +0200

    Changed number of skeletons

--- src/components/start-page/projects-section.tsx
+++ src/components/start-page/projects-section.tsx
@@ -137,7 +137,7 @@ const ProjectsSection: React.FC = () => {
 
         {projectsLoading ? (
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-            {[...Array(8)].map((_, i) => (
+            {[...Array(4)].map((_, i) => (
               <Card key={i} className="h-64">
                 <CardContent className="p-0">
                   <Skeleton className="h-32 w-full rounded-t-xl" />
