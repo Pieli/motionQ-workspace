1b42d6054aff4139dbff613d44ea614259505a48
Pieli <49155289+Pieli@users.noreply.github.com>
2025-08-19 17:53:36 +0200

    Fixed: typo

--- src/remotion-lib/animation-types.ts
+++ src/remotion-lib/animation-types.ts
@@ -5,14 +5,14 @@ import { animationMap, backgroundMap } from "@/remotion-lib/animation-bindings";
 // Animation Union of Schemas
 const schemas: z.ZodType[] = Object.values(animationMap).map((animation) => animation.llm_schema);
 if (schemas.length < 2) {
-    throw Error("Too few schemas provided... at leas 2 needed");
+    throw Error("Too few schemas provided... at least 2 needed");
 }
 export const UnionAvailableSchemas = z.union(schemas as [z.ZodTypeAny, z.ZodTypeAny, ...z.ZodTypeAny[]]);
 
 // Background Union of Schemas
 const backSchemas: z.ZodType[] = Object.values(backgroundMap).map((back) => back.llm_schema);
 if (backSchemas.length < 2) {
-    throw Error("Too few schemas provided... at leas 2 needed");
+    throw Error("Too few schemas provided... at least 2 needed");
 }
 export const UnionAvailableBackSchemas = z.union(backSchemas as [z.ZodTypeAny, z.ZodTypeAny, ...z.ZodTypeAny[]]);
 
